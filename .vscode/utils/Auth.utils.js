819c146d8a2a75b36153143ffdfb021069247415
ThanukshiWickramasinghe <thanukshi1234@gmail.com>
2021-05-04 22:44:51 +0530

    API for registration

--- .vscode/utils/Auth.utils.js
+++ .vscode/utils/Auth.utils.js
@@ -2,47 +2,46 @@ const User = require('../models/User');
 
 const userRegister = async (userDetails, role, res) => {
 	//check username available
-	let UsernameTaken = await checkAvailabilityOfUserName(userDetails);
+	let UsernameNotTaken = await checkAvailabilityOfUserName(userDetails);
 
-	if (!UsernameTaken) {
+	if (!UsernameNotTaken) {
 		return res.status(400).json({
 			success: false,
 			message: 'Username is already taken',
 		});
 	}
 	//check user email available
-	let UserEmailTaken = await checkAvailabilityOfUserEmail(userDetails);
+	let UserEmailNotTaken = await checkAvailabilityOfUserEmail(userDetails);
 
-	if (!UserEmailTaken) {
+	if (!UserEmailNotTaken) {
 		return res.status(400).json({
 			success: false,
 			message: 'Email is already taken',
 		});
 	}
+
+	//check user phone available
+	let UserPhoneNotTaken = await checkAvailabilityOfUserEmail(userDetails);
+
+	if (!UserPhoneNotTaken) {
+		return res.status(400).json({
+			success: false,
+			message: 'Phone Number is already taken',
+		});
+	}
 };
 
-// const checkAvailabilityOfUserName = async (username) => {
-// 	let user = User.findOne(username);
-
-// 	if (user) {
-// 		return {
-// 			success: false,
-// 			code: 400,
-// 		};
-// 	} else {
-// 		return {
-// 			success: true,
-// 			code: 200,
-// 		};
-// 	}
-// };
-
-const checkAvailabilityOfUserName = async (username) => {
-	let user = User.findOne(username);
+const checkAvailabilityOfUserName = async (use_name) => {
+	let user = User.findOne(use_name);
 	return user ? false : true;
 };
 
 const checkAvailabilityOfUserEmail = async (user_email) => {
 	let user = User.findOne(user_email);
+	return user ? false : true;
+};
+
+const checkAvailabilityOfUserPhone = async (user_phone) => {
+	let user = User.findOne(user_phone);
 	return user ? false : true;
 };
