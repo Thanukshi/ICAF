f78b3b57443c0fea3e28e9e9547ae0c86f219e22
ThanukshiWickramasinghe <thanukshi1234@gmail.com>
2021-05-16 11:54:12 +0530

    Sever Edited By IT18202632

--- server/server.js
+++ server/server.js
@@ -5,13 +5,17 @@ require('dotenv').config({
 	path: './config/config.env',
 });
 const PORT = process.env.PORT;
-//const cors = require('cors');
-
-if (process.env.NODE_ENV === 'development') {
-    app.use()
-}
+const cors = require('cors');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({
+		origin:process.env.CLIENT_URL
+	}))
+}
+
+
 
 app.listen(PORT, () => {
 	console.log(`Sever is started on ${PORT}`);
