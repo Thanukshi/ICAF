22a047fd79253fdde9ae5679ef5d52b286518891
ThanukshiWickramasinghe <thanukshi1234@gmail.com>
2021-05-16 12:19:47 +0530

    Sever Edited By IT18202632

--- server/server.js
+++ server/server.js
@@ -10,12 +10,21 @@ const cors = require('cors');
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(cors({
-		origin:process.env.CLIENT_URL
-	}))
+	app.use(
+		cors({
+			origin: process.env.CLIENT_URL,
+		})
+	);
 }
 
+app.use(morgan('dev'));
+//Can get more information about request
 
+app.use((req, res, next) => {
+	res.status(400).json({
+
+	});
+});
 
 app.listen(PORT, () => {
 	console.log(`Sever is started on ${PORT}`);
