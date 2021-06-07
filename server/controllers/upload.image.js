9aebad65beb1c505bbbb9a9512bdf9d69503e7aa
Thanukshi <thanukshi1234@gmail.com>
2021-06-07 12:53:12 +0530

    ICAF

--- server/controllers/upload.image.js
+++ server/controllers/upload.image.js
@@ -1,11 +1,57 @@
 const cloudinary = require("cloudinary");
 const fs = require("fs");
+const messages = require("../messages/messages");
+const User = require("../models/user.model");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const uploadImageController = {
   uploadAvatar: async (req, res) => {
     try {
-    } catch (err) {}
+      const uploadFile = req.files.file;
+      cloudinary.v2.uploader.upload(
+        uploadFile.tempFilePath,
+        {
+          folder: "icaf_avatar",
+          width: 150,
+          height: 150,
+          crop: "fill",
+        },
+        async (err, result) => {
+          if (err) throw err;
+
+          removeTmp(uploadFile.tempFilePath);
+
+          const user = await User.findById(req.user.id).select("-password");
+          return res.status(200).json({
+            code: messages.SuccessCode,
+            success: messages.Success,
+            status: messages.SuccessStatus,
+            data: user,
+            result: result,
+            message: "Image uploaded successfully.",
+          });
+        }
+      );
+    } catch (err) {
+      return res.status(500).json({
+        code: messages.InternalCode,
+        success: messages.NotSuccess,
+        status: messages.InternalStatus,
+        message: err.message,
+      });
+    }
   },
+};
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
 
 module.exports = uploadImageController;
--- server/routes/uploadRoutes/index.js
+++ server/routes/uploadRoutes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = require("express").Router();
 const uploadImageMiddleware = require("../../middleware/upload.middleware");
 const uploadImageController = require("../../controllers/upload.image");
+const auth_middleware = require("../../middleware/auth");
 
 router.post(
   "/upload-avatar",
   uploadImageMiddleware,
+  auth_middleware,
   uploadImageController.uploadAvatar
 );
 
