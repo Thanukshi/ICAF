f9c9a3f6ad13ff6420699c6781a66d0bb88a6542
Mishane Perera <44816836+mish524@users.noreply.github.com>
2021-07-01 03:29:43 +0530

    IT18198232

---
+++ client/src/Component/Dashboard/Admin/Admin.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import "../../../../css/dashboard.css";
+import { authDetail } from "../../../common/config";
+import profileIcon from "../../../../Images/user.png";
+
+const initialState = {
+  user: [],
+};
+
+class Admin extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+
+  componentDidMount() {
+    let user = {};
+    axios
+      .get("http://localhost:8000/users/get-user-details", {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        this.setState(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return user;
+  }
+
+  render() {
+    return (
+      <div id="wrapper">
+        <aside id="sidebar-wrapper">
+          <div className="sidebar-brand">
+            <h2>{this.state.user_name}</h2>
+            <h4>{this.state.role}</h4>
+          </div>
+          <ul className="sidebar-nav">
+            <li className="active">
+              <a href="/research-dash">
+                <i className="fa fa-home"></i>Home
+              </a>
+            </li>
+            <li>
+              <a href="/research-dash-paper">
+                <i className="fa fa-plus"></i>Reseach Papers
+              </a>
+            </li>
+            <li>
+              <a href="/research-dash-profile">
+                <i className="fa fa-user"></i>Profile
+              </a>
+            </li>
+          </ul>
+        </aside>
+
+        <div id="navbar-wrapper">
+          <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                {/* <a href="#" className="navbar-brand" id="sidebar-toggle">
+                <i className="fa fa-bars"></i>
+              </a> */}
+                <div>
+                  <img className="profile-img" src={profileIcon} alt="" />
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <section id="content-wrapper">
+          <div className="row">
+            <div className="col-lg-12">
+              <h2 className="content-title">WorkShopPresenter</h2>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Admin;
